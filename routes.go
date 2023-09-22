e0b7797f72af5a6afb42e5547e9aa73f51bcf856
lxhan <lxhan@naver.com>
2023-09-22 16:14:38 +0900

    test date

--- routes.go
+++ routes.go
@@ -41,7 +41,10 @@ func Daily(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 
 	blockId := blocks[0].Id
 
-	now := time.Now()
+	now, err := TimeIn(time.Now(), "Asia/Seoul")
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	title := now.Format("Monday, 02/01/2006")
 
